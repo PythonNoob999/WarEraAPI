942af4de89c12cdec54ef18c5d26d766601ac7c7
PythonNoob999 <kerolisand@gmail.com>
2026-05-06 06:07:39 +0300

    add missing trading_tx fields

--- src/WarEraAPI/types/transactions/trading_tx.py
+++ src/WarEraAPI/types/transactions/trading_tx.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from datetime import datetime
 from typing import Literal
 
@@ -16,9 +16,16 @@ class TradingTransaction:
     sellerId: str
     buyerId: str
     transactionType: Literal["trading"]
-    offerCreatedAt: datetime
     createdAt: datetime
     updatedAt: datetime
+    buyerMuId: str | None = field(default=None)
+    buyerPartyId: str | None = field(default=None)
+    buyerCountryId: str | None = field(default=None)
+    sellerMuId: str | None = field(default=None)
+    sellerPartyId: str | None = field(default=None)
+    sellerCountryId: str | None = field(default=None)
+    processedByModAt: datetime | None = field(default=None)
+    offerCreatedAt: datetime | None = field(default=None)
 
 
     def __post_init__(self):
